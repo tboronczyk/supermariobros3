65112ebd65e1c2f18a1967f92d323c8d8f985fa8
Timothy Boronczyk <tboronczyk@gmail.com>
2022-10-23 00:05:17 -0400

    status bar

--- build.sh
+++ build.sh
@@ -14,6 +14,8 @@ cp smb3.nes ../$ROM
 cd ..
 
 echo "Updating CHR..."
+dd if=chr/status-spesmilo.bin of="$ROM" conv=notrunc bs=1 seek=$((0x57ED0))
+dd if=chr/status-world.bin of="$ROM" conv=notrunc bs=1 seek=$((0x57710))
 dd if=chr/title-d.bin of="$ROM" conv=notrunc bs=1 seek=$((0x5E0B0))
 dd if=chr/title-u.bin of="$ROM" conv=notrunc bs=1 seek=$((0x5E0E0))
 dd if=chr/title-t.bin of="$ROM" conv=notrunc bs=1 seek=$((0x5E1B0))
